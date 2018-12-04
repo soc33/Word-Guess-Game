b115e0e3ae9fcf9d75acf40dfe67a20860d47ccf
soc33 <soc33@msn.com>
2018-12-03 23:55:13 -0500

    almost done, pictures working, text working, still need recipes

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,8 +1,9 @@
 //empty variables for stats
 var wins = 0;
 var losses = 0;
-var guessesLeft = 20;
+var guessesLeft = 10;
 var guessedLetters = [];
+
 // empty variables for grabbing the HTML text
 var answerText = document.getElementById("answerText");
 var picture = document.getElementById("picture");
@@ -12,62 +13,94 @@ var wordText = document.getElementById("word");
 var guessesLeftText = document.getElementById("guessesLeft");
 var guessedLettersText = document.getElementById("lettersUsed");
 
+// global variables to be used in functions
 var computerChoices = ["tomato", "apple", "carrot", "watermelon", "pear"];
+var j = "";
+var computerWord = "tomato";
+var userGuess = "";
+var pictures = ["assests/images/tomatoes.jpg", "assests/images/apples.jpg", "assests/images/colorfulcarrots.jpg", "assests/images/Watermelon.jpg", "assests/images/pears.jpg", "assests/images/winner.jpg"];
+var flavorText = ["Tomato soup is my favorite soup!", "Cinnamon roll apple pie is the best thing you've ever had!", "The best carrots are multicolored", "Watermelon is the best fruit for centerpieces", "Pears are best served cold", "You solved all the words! Refresh the page to play again."];
+var recipes = [];
+var usedWords = [];
 
-var computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerWord);
-// var wordLength = computerWord.length;
-var j;
-for (j = ""; j.length <= computerWord.length; j += "_") {
+// function to get new word for blanks
+getnewword = function () {
+    computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerWord);
+    j = "";
+    for (i = 0; i < computerWord.length; i++) {
+        j += "_";
+    }
     wordText.textContent = j;
 };
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-    if (guessesLeft != 1) {
-        if (computerWord.indexOf(userGuess) > -1) {
-            if (chosenWord.indexOf(keyString) != -1){ // if the character is found
-                for (var i = 0; i < wordLength; i ++){ // loop on all characters
-                    if (chosenWord[i] == keyString) { // if this is an occurance
-                        progressWord[i] = chosenWord[i];
-                    }
-                }
-            }else{
-             // wrong choice
-            }
-            //for (var i = 0; i < computerWord.length; i++) {
-              //  if (computerWord.charAt(i) === userGuess) {
-                //    var letter = computerWord.charAt(i);
-                  //  j.prototype.CharAt(i) = letter;
-                    if (j === computerWord) {
-                        wins++;
-                        winsText.textContent = wins;
-                        computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-                        console.log(computerWord);
-                        for (j = ""; j.length <= computerWord.length; j += "_") {
-                            wordText.textContent = j;
-                        }
-                    }
-                }
-            }
-            //this part works!
-        } else {
-            guessesLeft--;
-            guessesLeftText.textContent = guessesLeft;
-            guessedLetters.push(userGuess);
-            guessedLettersText.textContent = guessedLetters;
+
+// function to replace the blank with the guessed letter
+replaceAt = function (text, index, replacement) {
+    return text.substr(0, index) + replacement + text.substr(index + replacement.length);
+};
+
+//function to find the correct blank and then use replace function to replace it correctly
+setCharacter = function () {
+    for (var i = 0; i < computerWord.length; i++) { // loop on all characters
+        if (computerWord[i] == userGuess) { // if this is an occurance
+            j = replaceAt(j, i, userGuess);
         }
-    } else {
-// this part works too!!
-        losses++;
-        lossesText.textContent = ("Losses: " + losses);
-        guessedLetters = [];
-        guessedLettersText.textContent = guessedLetters;
-        guessesLeft = 20;
-        guessesLeftText.textContent = guessesLeft;
-        computerWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log(computerWord);
-        for (j = ""; j.length <= computerWord.length; j += "_") {
-            wordText.textContent = j;
-        };
+    }
+    wordText.textContent = j;
+};
+
+//function to "reset" the game
+resetGame = function () {
+    guessedLetters = [];
+    guessedLettersText.textContent = guessedLetters;
+    guessesLeft = 10;
+    guessesLeftText.textContent = guessesLeft;
+    getnewword();
+    if (usedWords.indexOf(computerWord) != -1) {
+        getnewword();
     }
 }
+
+//to get a word upon page load
+getnewword();
+
+// function to run game upon a release or a key press
+document.onkeyup = function (event) {
+    userGuess = event.key;
+    if (userGuess >= 'a' && userGuess <= 'z') {
+        if (guessedLetters.indexOf(userGuess) === -1) {
+            if (guessesLeft != 1) { //if you have guesses left
+                if (computerWord.indexOf(userGuess) > -1) { // if the character is found
+                    setCharacter();
+                    if (j === computerWord) {
+                        wins++;
+                        winsText.textContent = ("Wins: " + wins);
+                        picture.src=pictures[computerChoices.indexOf(computerWord)];
+                        answerText.textContent = flavorText[computerChoices.indexOf(computerWord)];
+                        usedWords.push(computerWord);
+                        resetGame();
+                        if (wins === 5) {
+                            picture.src=pictures[pictures.length -1];
+                            answerText.textContent = flavorText[flavorText.length -1];
+                        }
+                    }
+                } else {//this part works!
+                    guessesLeft--;
+                    guessesLeftText.textContent = guessesLeft;
+                    guessedLetters.push(userGuess);
+                    guessedLettersText.textContent = guessedLetters;
+                }
+            } else {
+                // this part works too!!
+                losses++;
+                lossesText.textContent = ("Losses: " + losses);
+                resetGame();
+            };
+        } else {
+            alert("You've already guessed that letter!");
+        }
+    } else {
+        alert("That is not a valid choice!");
+    }
+}
+
